2790b20ad01f3e5a8c245cf3acd75ef4ff57f811
Ishaan vats <Ishaanvats74@gmail.com>
2025-07-11 14:20:17 +0530

    added loading screen

--- app/Profile/[[...rest]]/page.tsx
+++ app/Profile/[[...rest]]/page.tsx
@@ -1,24 +1,45 @@
 'use client';
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UserInfo from '../../Components/UserInfo';
 import OrderHistory from '../../Components/OrderHistory';
 import AddressSection from '@/app/Components/AddressSection';
 import Setting from '@/app/Components/Setting';
+import { ShoppingBag,  } from 'lucide-react'; 
 
 
-const page = () => {
+const Page = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    const timeout = setTimeout(()=> {setLoading(false)},10);
+    return ()=> clearTimeout(timeout);
+  },[]);
+ 
+
   return (
+    <>
+    {loading && (
+            <div className="fixed inset-0 flex flex-col items-center justify-center bg-gradient-to-br from-orange-50 to-yellow-100 z-50">
+      <div className="relative animate-bounce mb-4">
+        <div className="w-20 h-20 bg-orange-500 rounded-full shadow-xl flex items-center justify-center">
+          <ShoppingBag className="text-white w-10 h-10" />
+        </div>
+      </div>
+      <div className="mt-4 text-orange-800 text-md font-medium animate-fade-in-up">
+        Almost there... 🛍️✨
+      </div>
+    </div>
+    )}
    <div className="max-w-4xl mx-auto py-10 space-y-8">
-  <h2 className="text-2xl font-semibold mb-6">Your Profile</h2>
-
-  
-    <UserInfo/>
-    <OrderHistory />
-    <AddressSection />
-    <Setting />
-</div>
+      <h2 className="text-2xl font-semibold mb-6">Your Profile</h2>
+        <UserInfo/>
+        <OrderHistory />
+        <AddressSection />
+        <Setting />
+    </div>
+    </>
   )
 }
 
-export default page
+export default Page
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,37 @@
 'use client';
-
+import { ShoppingBag } from "lucide-react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+
+    const timeout = setTimeout(()=>setLoading(false),10);
+    return ()=> clearTimeout(timeout);
+  },[])
+  
+
+
+    
+  
+
   return (
+    <>
+    {loading &&(
+            <div className="fixed inset-0 flex flex-col items-center justify-center bg-gradient-to-br from-orange-50 to-yellow-100 z-50">
+      <div className="relative animate-bounce mb-4">
+        <div className="w-20 h-20 bg-orange-500 rounded-full shadow-xl flex items-center justify-center">
+          <ShoppingBag className="text-white w-10 h-10" />
+        </div>
+      </div>
+      <div className="mt-4 text-orange-800 text-md font-medium animate-fade-in-up">
+        Almost there... 🛍️✨
+      </div>
+    </div>
+    )}
     <div className="relative w-full h-[600px] overflow-hidden">
       <div className=" z-0">
       <Image src="/images/PC_Hero_Asin_3000x1200---Prime-Blue-TRIMMER-PDED._CB790427816_.jpg" width={1000} height={1000} alt="" className="w-full h-full object-cover z-0"/>
@@ -12,5 +39,6 @@ export default function Home() {
       <div className="grid grid-cols-4 grid-rows-2 z-50">
       </div>
     </div>
+    </>
   );
 }
