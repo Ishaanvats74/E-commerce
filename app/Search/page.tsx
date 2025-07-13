b5a6bee85114a3cd58fffb14780369df47782044
Ishaanvats74 <Ishaanvats74@gmail.com>
2025-07-13 15:26:31 +0530

    vercel issue

--- app/Search/page.tsx
+++ app/Search/page.tsx
@@ -4,7 +4,7 @@ import { Rating } from '@smastrom/react-rating';
 import '@smastrom/react-rating/style.css';
 import Image from 'next/image'
 import { useSearchParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 
 type Product = {
   id: number;
@@ -41,60 +41,62 @@ const Page = () => {
   },[query]);
   
   return (
-    <div className='flex max-w-full mt-5 h-auto'>
-      <div className='w-[20%] px-5 space-y-5 '>
-        <div>
-            <div className='font-bold text-xl'>Year</div> 
-            <div className='flex flex-col gap-2 '>
-                <div className='flex items-center gap-1'>
-                    <input type='checkbox' id='2012-1' name='2012-1' className='h-3 w-3'/>
-                    <label htmlFor="2012-1">2012</label>
-                </div>
-                <div className='flex items-center gap-1' >
-                    <input type='checkbox' id='2012-2' name='2012-2' className='h-3 w-3'/>
-                    <label htmlFor="2012-2">2012</label>
-                </div>
-                <div className='flex items-center gap-1'>
-                    <input type='checkbox' id='2012-3' name='2012-3' className='h-3 w-3'/>
-                    <label htmlFor="2012-3">2012</label>
-                </div>
-            </div>
+    <Suspense fallback={<div className="p-10 text-xl">Loading...</div>}>
+      <div className='flex max-w-full mt-5 h-auto'>
+        <div className='w-[20%] px-5 space-y-5 '>
+          <div>
+              <div className='font-bold text-xl'>Year</div> 
+              <div className='flex flex-col gap-2 '>
+                  <div className='flex items-center gap-1'>
+                      <input type='checkbox' id='2012-1' name='2012-1' className='h-3 w-3'/>
+                      <label htmlFor="2012-1">2012</label>
+                  </div>
+                  <div className='flex items-center gap-1' >
+                      <input type='checkbox' id='2012-2' name='2012-2' className='h-3 w-3'/>
+                      <label htmlFor="2012-2">2012</label>
+                  </div>
+                  <div className='flex items-center gap-1'>
+                      <input type='checkbox' id='2012-3' name='2012-3' className='h-3 w-3'/>
+                      <label htmlFor="2012-3">2012</label>
+                  </div>
+              </div>
+          </div>
         </div>
-      </div>
 
-      <div className='w-[80%]'>
-        <div>
-            <div className='font-bold text-2xl'>Results for `{query}`</div>
-              {products.map((item)=>(
-                <div className='flex' key={item.id}>
-                <div>
-                    <Image src={item.link} alt={item.productDisplayName} width={300} height={300} className=''></Image>
-                </div>
-                <div>
-                    <div>
-                      <p>{item.productDisplayName}</p>
-                    </div>
-                    <div>
-                        <Rating style={{ maxWidth: 100 }} value={4} readOnly />
-                    </div>
-                    <div>
-                         <p>300+ bought in past month</p>
-                    </div>
-                    <div>
-                        <p className='text-2xl font-bold'>₹{item.price}</p>
-                    </div>
-                    <div>
-                        <p>(40% off)</p>
-                    </div>
-                    <div>
-                        <p>Save Extra with No Cost EMI</p>
-                    </div>
-                </div>
-            </div>
-              ))}
+        <div className='w-[80%]'>
+          <div>
+              <div className='font-bold text-2xl'>Results for `{query}`</div>
+                {products.map((item)=>(
+                  <div className='flex' key={item.id}>
+                  <div>
+                      <Image src={item.link} alt={item.productDisplayName} width={300} height={300} className=''></Image>
+                  </div>
+                  <div>
+                      <div>
+                        <p>{item.productDisplayName}</p>
+                      </div>
+                      <div>
+                          <Rating style={{ maxWidth: 100 }} value={4} readOnly />
+                      </div>
+                      <div>
+                          <p>300+ bought in past month</p>
+                      </div>
+                      <div>
+                          <p className='text-2xl font-bold'>₹{item.price}</p>
+                      </div>
+                      <div>
+                          <p>(40% off)</p>
+                      </div>
+                      <div>
+                          <p>Save Extra with No Cost EMI</p>
+                      </div>
+                  </div>
+              </div>
+                ))}
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   )
 }
 
